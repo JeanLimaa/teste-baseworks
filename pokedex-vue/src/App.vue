b7f926c7b31790edfccdc77e7a21646dd7c48554
Jean Lima <jeansantoslima17@gmail.com>
2024-10-01 21:09:19 -0300

    chore: initial project setup with routing and services

---
+++ pokedex-vue/src/App.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+</script>
+<template>
+  <main>
+    <RouterView />
+  </main>
+</template>
+
+
+<style scoped>
+h1 {
+  text-align: center;
+  margin-bottom: 20px;
+}
+body {
+  margin: 0;
+  padding: 0;
+}
+
+main {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: aliceblue;
+}
+</style>
